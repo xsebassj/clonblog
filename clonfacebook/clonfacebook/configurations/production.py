0e12f3a787eaa9bda3b474c12bc55fde035a28f0
xsebassj <tian10122017@gmail.com>
2025-08-02 22:25:31 -0300

    algunos cambios

--- clonfacebook/clonfacebook/configurations/production.py
+++ clonfacebook/clonfacebook/configurations/production.py
@@ -1,7 +1,6 @@
 from .base import *
-DEBUG:False
-ALLOWED_HOSTS = [ 'localhost',	 '3000']
-
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
